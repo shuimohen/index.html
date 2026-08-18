0a43a8c1bda51b05c699c01b22348378bd89d5da
shuimohen <39368522+shuimohen@users.noreply.github.com>
2026-08-18 16:12:26 +0800

    Update p5.js

--- functions/api/p5.js
+++ functions/api/p5.js
@@ -1,8 +1,8 @@
 export async function onRequest({ request }) {
   const key = "f44a7660881514c58ed987ca5ab934f0";
   const caipiaoid = "17"; //排列五
-  // ✅ 恢复官方原生路由，不要/list
-  const url = `https://api2.tanshuapi.com/api/caipiao/v1/winning?key=${key}&caipiaoid=${caipiaoid}&refernumber=100`;
+  // ✅ 重点：换成 v1/query 历史列表接口，不是 /winning
+  const url = `https://api2.tanshuapi.com/api/caipiao/v1/query?key=${key}&caipiaoid=${caipiaoid}&refernumber=100`;
 
   const res = await fetch(url);
   const data = await res.json();

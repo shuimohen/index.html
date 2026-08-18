111dca03b997f6a06ea07e4d9b568b9c3660e18f
shuimohen <39368522+shuimohen@users.noreply.github.com>
2026-08-18 16:40:29 +0800

    Update p5.js

--- functions/api/p5.js
+++ functions/api/p5.js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
   const key = "f44a7660881514c58ed987ca5ab934f0";
   const caipiaoid = "17";
-  // ✅ 改用 v1/history 接口，取100期
+  // ✅ num=100 拉取近100期
   const url = `https://api2.tanshuapi.com/api/caipiao/v1/history?key=${key}&caipiaoid=${caipiaoid}&issueno=&start=0&num=100`;
 
   const res = await fetch(url);
